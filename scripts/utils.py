0b44327eb8bb73b508193a5f44eed1421b1a9ef1
cham <cham@vectra.ai>
2020-01-09 15:35:19 -0600

    reduce logging 'info' mode ouput
    more detailed information printed in 'debug' mode

--- scripts/utils.py
+++ scripts/utils.py
@@ -9,22 +9,21 @@ def retrieve_hosts(args, db):
 
     if args.get('tags', None):
         hosts = vc.get_hosts(tags=args['tags']).json()
-        logging.info("{count} hosts returned with tags: {tags}".format(count=hosts['count'], tags=args['tags']))
+        logging.debug("{count} hosts returned with tags: {tags}".format(count=hosts['count'], tags=args['tags']))
     if args.get('certainty_gte', None) or args.get('threat_gte', None):
         hosts = vc.get_hosts(certainty_gte=args.get('certainty_gte', 50), threat_gte=args.get('threat_gte', 50)).json()
-        logging.info("{count} hosts returned with score: certainty {certainty} threat {threat}".format(count=hosts['count'], 
+        logging.debug("{count} hosts returned with score: certainty {certainty} threat {threat}".format(count=hosts['count'], 
             certainty=args.get('certainty_get', 50), threat=args.get('threat_get', 50)))
 
     for host in hosts['results']:
-        logging.info('host_id:{}, name:{}, ip:{}'.format(host['id'], host['name'], host['last_source']))
+        logging.debug('host_id:{}, name:{}, ip:{}'.format(host['id'], host['name'], host['last_source']))
         db.insert({"id": host['id'], "name": host['name'], 'ip': host['last_source']})
-        logging.info('host ' + host['name'] + ':' + host['last_source'] + ' added to block list')
+        logging.debug('host ' + host['name'] + ':' + host['last_source'] + ' added to block list')
 
     return
 
 
 def retrieve_detections(args, db):
-    logging.info("retrieve_detections called")
     vc = vectra.VectraClient(url=args['url'], token=args['token'])
 
     if bool(args.get('state', None)) and bool(args.get('triaged', None)):
@@ -53,6 +52,6 @@ def retrieve_detections(args, db):
 
         logging.debug('det_id:{}, dst_ips:{}'.format(detection['id'], ips))
         db.insert({"id": detection['id'], 'dst_ips': ips})
-        logging.info(str(ips) + ' added to block list')
+        logging.debug(str(ips) + ' added to block list')
 
     return
